630a2560efbeec41687519516d96b69054b5f253
timon <nobelentimon@gmail.com>
2025-04-29 10:44:33 +0200

    feat: add tailwindcss to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
+
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
@@ -10,4 +17,4 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/ui'
   ]
-})
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="bg-black text-white p-4">Welcome to the homepage</h1>
+  </div>
+</template>
